2733052fec4b83c3ecebbe7ed835e638c5fde6ba
Octavio <opaguaga@gmail.com>
2014-09-09 16:43:43 -0400

    Added new script pythonGrep.py

---
+++ pythonGrep.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+
+""" Python Grep will mimic grep in bash. Purpose of this script is to fulfill one of the challenges of the OSCP exam"""
+
+
+
+def main():
+    print 'placeholder'
+  
+
+if __name__ == '__main__':
+  main()
