767275f49d88ab6b6c7c02a3161c687f42d6cc74
Octavio <opaguaga@gmail.com>
2015-06-23 18:03:33 -0400

    Parser change plus more notes
    Updated nessus_csv_parser. Added logic that took into consideration
    plugin_ids that had different port numbers.

--- nessus_csv_parser.py
+++ nessus_csv_parser.py
@@ -16,18 +16,21 @@ call match whatever the first row of the csv file are equal too.  Look at
 PMOTW '''
 '''useful link http://www.youlikeprogramming.com/2013/11/python-csv-reader-dictreader-quick-reference/ #Quote char'''
 def parsecsv(fileName):     
-    prev_id='0'     
+    prev_id='0'
+    prev_port=''
+    prev_protocol=''     
     current_id='' 
-    host=[]
+    hostList=[]
     #csvfile=open(fileName, 'rt')     
     with open(fileName, 'rb') as csvfile:
         myfile=csv.DictReader(csvfile,quotechar='"') 
         for row in myfile:
             current_id=row['Plugin ID']
+            current_protocol=row['Protocol']
+            current_port=row['Port']
             if prev_id == '0':
-                print 'first entry'
-                host.append(row['Host'])
-                lineentry='%s","%s","%s"' %(row['Plugin ID'],row['Name'],row['Synopsis'])             
+                hostList.append(row['Host'])
+                #lineentry='"%s","%s","%s,"%s","%s"' % (row['Plugin ID'],row['Name'],row['Synopsis'],row['Protocol'],row['Port'])
             elif prev_id != current_id: 
                 '''Review for If the condition above is met, print out lineentry
                 variable as well as all the IP's currently populated in the host array. Empty
@@ -35,19 +38,24 @@ def parsecsv(fileName):
                 array. '''                 
                 '''In the below print statment, the lineentry variable doesn't need quotes since those were added when line
                 entry variable was created was created.'''
-                print '%s,"%s"' % (lineentry, ' '.join(host))                 
-                host=[]
-                host.append(row['Host'])                
+                print '%s,"%s"' % (lineentry, '; '.join(hostList))                 
+                hostList=[]
+                hostList.append(row['Host'])                
             elif prev_id==current_id: 
                 '''If condition above is met, the only thing that has to be done is to add the host's IP
                 address to the  host list. '''
-                host.append(row['Host'])
-                lineentry='"%s","%s","%s"' % (row['Plugin ID'],row['Name'],row['Synopsis'])
-                '''At the completion of the for loop finish out the logic only if the current_id and prev_id are equal.  
-                since that statement in the above for loop does not have a print statement we need this print statement'''
-            prev_id=current_id 
+                if (prev_port != current_port) or (prev_protocol != current_protocol):
+                    print '%s,"%s"' % (lineentry, '; '.join(hostList))                 
+                    hostList=[]
+                    hostList.append(row['Host'])                
+            lineentry='"%s","%s","%s,"%s","%s"' % (row['Plugin ID'],row['Name'],row['Synopsis'],row['Protocol'],row['Port'])
+            prev_id=current_id
+            prev_port=current_port
+            prev_protocol=current_protocol
+        '''At the completion of the for loop finish out the logic only if the current_id and prev_id are equal.  
+        since that statement in the above for loop does not have a print statement we need this print statement'''
         if prev_id==current_id:
-            print '"%s","%s","%s","%s"' % (row['Plugin ID'],row['Name'],row['Synopsis'],''.join(host))
+            print '"%s","%s","%s,"%s","%s","%s"' % (row['Plugin ID'],row['Name'],row['Synopsis'],row['Protocol'],row['Port'],'; '.join(hostList))
         csvfile.close()
 
 def main():
